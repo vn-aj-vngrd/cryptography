43c011104b9ee6448f4a786372829028683bee77
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-11-11 02:15:48 +0800

    update

--- PolyAlphabetic/test.c
+++ PolyAlphabetic/test.c
@@ -14,8 +14,8 @@ char *decrypt(char[], char[]);
 
 int main()
 {
-    char text[MAX_SIZE];
-    char key_val[MAX_SIZE];
+    char text[MAX_SIZE] = "van aj";
+    char key_val[MAX_SIZE] = "ed";
     int choice;
 
     do
@@ -27,16 +27,16 @@ int main()
 
         if (choice == 1)
         {
-            printf("Input text: ");
-            scanf("%[^\n]", &text);
-            fflush(stdin);
+            // printf("Input text: ");
+            // scanf("%[^\n]", &text);
+            // fflush(stdin);
 
-            printf("Input key: ");
-            scanf("%[^\n]", &key_val);
-            fflush(stdin);
+            // printf("Input key: ");
+            // scanf("%[^\n]", &key_val);
+            // fflush(stdin);
 
             char *new_key = generateKey(text, key_val);
-            // printf("New Key: %s", new_key);
+            printf("New Key: %s", new_key);
 
             char *encrypted_text = encrypt(text, new_key);
             printf("\nEncrypted Text: %s", encrypted_text);
@@ -52,7 +52,7 @@ int main()
             fflush(stdin);
 
             char *new_key = generateKey(text, key_val);
-            // printf("New Key: %s", new_key);
+            printf("New Key: %s", new_key);
 
             char *decrypted_text = decrypt(text, new_key);
             printf("\nDecrypted Text: %s", decrypted_text);
@@ -92,7 +92,7 @@ char *generateKey(char plain_text[], char key_val[])
         if (j == strlen(key_val))
             j = 0;
 
-        new_key[i] = plain_text[i] == ' ' ? ' ' : toupper(key_val[j++]);
+        new_key[i] = plain_text[i] == ' ' ? ' ' : key_val[j++];
     }
 
     return new_key;
@@ -108,8 +108,10 @@ char *encrypt(char plain_text[], char key[])
 
         if (islower(plain_text[i]))
         {
-            encrypted_text[i] = ((toupper(plain_text[i]) + key[i]) % MAX_ALPHA) + 'a';
-            encrypted_text[i] = tolower(encrypted_text[i]);
+            // encrypted_text[i] = ((toupper(plain_text[i]) + key[i]) % MAX_ALPHA) + 'a';
+            // encrypted_text[i] = tolower(encrypted_text[i]);
+
+            encrypted_text[i] = ((plain_text[i] + key[i]) % MAX_ALPHA) + 'a';
         }
         else if (isupper(plain_text[i]))
         {
@@ -133,8 +135,10 @@ char *decrypt(char cipher_text[], char key[])
 
         if (islower(cipher_text[i]))
         {
-            decrypted_text[i] = ((toupper(cipher_text[i]) - key[i] + MAX_ALPHA) % MAX_ALPHA) + 'a';
-            decrypted_text[i] = tolower(decrypted_text[i]);
+            // decrypted_text[i] = ((toupper(cipher_text[i]) - key[i] + MAX_ALPHA) % MAX_ALPHA) + 'a';
+            // decrypted_text[i] = tolower(decrypted_text[i]);
+
+            decrypted_text[i] = ((cipher_text[i] - key[i] + MAX_ALPHA) % MAX_ALPHA) + 'a';
         }
         else if (isupper(cipher_text[i]))
         {
