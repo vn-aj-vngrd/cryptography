a553ceb484b596ae718ced60da7bdf7c0dfef7c9
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-11-14 11:08:14 +0800

    update docs

--- Vernam/vernam_Vanguardia.c
+++ Vernam/vernam_Vanguardia.c
@@ -34,31 +34,11 @@ int main()
             }
             else
             {
-                char *encrypted_text = vernam(text, key_val);
-                printf("Encrypted Text: %s", encrypted_text);
+                char *transformed_text = vernam(text, key_val);
+                printf("Encrypted Text: %s", transformed_text);
             }
         }
         else if (choice == 2)
-        {
-            printf("Input text: ");
-            scanf("%[^\n]", &text);
-            fflush(stdin);
-
-            printf("Input key: ");
-            scanf("%[^\n]", &key_val);
-            fflush(stdin);
-
-            if (strlen(key_val) < strlen(text))
-            {
-                printf("Error: The key must be of the same length or greater as the text.");
-            }
-            else
-            {
-                char *decrypted_text = vernam(text, key_val);
-                printf("Decrypted Text: %s", decrypted_text);
-            }
-        }
-        else if (choice == 3)
         {
             printf("Thank you for using this program.");
             break;
@@ -79,9 +59,8 @@ int menu()
 {
     int choice;
 
-    printf("[1] Encrypt\n");
-    printf("[2] Decrypt\n");
-    printf("[3] Exit\n");
+    printf("[1] Start\n");
+    printf("[2] Exit\n");
     printf("\nSelect: ");
 
     scanf("%d", &choice);
@@ -113,7 +92,7 @@ int menu()
 
 char *vernam(char plain_text[], char key[])
 {
-    char *cipher_text = (char *)calloc((strlen(plain_text)) + 1, sizeof(char));
+    char *transformed_text = (char *)calloc((strlen(plain_text)) + 1, sizeof(char));
     int text_val, key_val, cipher_val;
 
     int i;
@@ -123,8 +102,8 @@ char *vernam(char plain_text[], char key[])
         key_val = toupper(key[i]) - 'A';
 
         cipher_val = text_val ^ key_val;
-        cipher_text[i] = islower(plain_text[i]) ? cipher_val + 'a' : cipher_val + 'A';
+        transformed_text[i] = islower(plain_text[i]) ? cipher_val + 'a' : cipher_val + 'A';
     }
 
-    return cipher_text;
+    return transformed_text;
 }
