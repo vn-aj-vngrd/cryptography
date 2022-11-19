94af3956efd353e4572ffb95b8cfb32a820efb7e
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-11-19 12:11:49 +0800

    remove

--- main.py
+++
@@ -1,8 +0,0 @@
-from vernamcipher.cryptographic import Cryptographic
-
-encrypted = "JKYRXRYAHSL"
-key = Cryptographic.generate_key()
-
-decrypted_data = Cryptographic.exclusive_operations(encrypted, key)
-
-print(decrypted_data)
